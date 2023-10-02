b1cd2efdd930ec2e4f977da64f66eff88046e1c9
Yures1lva <yure.siilva9@gmail.com>
2023-10-02 16:56:56 -0300

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1758FF",
+        dark: "#000510",
+        secundary: "17F1FF",
+        light: "#FFFFFF",
+        support01: "A0A0A0",
+        support02: "191D29",
+      },
+
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+  },
+};
