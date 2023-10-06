ffa5f24d808b45b1131ce804817c8c2109ea4f23
yuro <yure.siilva@gmail.com>
2023-10-06 15:39:40 -0300

    navbar simples

---
+++ MfProdutora/src/components/nav/Nav.tsx
@@ -0,0 +1,22 @@
+import styles from "./Nav.module.css"
+
+
+export function Nav(){
+    return (
+        <>
+            <header>
+                <nav>
+                    <a className={styles.logo} href=""><img src="" alt="logo" />logo</a>
+                    <ul>
+                    <li><a  href="">Início</a></li>
+                    <li><a  href="">Resultados</a></li>
+                    <li><a  href="">Serviços</a></li>
+                    <li><a  href="">Equipe</a></li>
+                    <li><a  href="">Contato</a></li>
+                    <li><button>Orçamento</button></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
