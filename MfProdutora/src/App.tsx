0ba4aebc4b945cf3b218886924dab154721c51a8
yuro <yure.siilva@gmail.com>
2023-10-06 17:04:15 -0300

    top-content implementado

--- MfProdutora/src/App.tsx
+++ MfProdutora/src/App.tsx
@@ -1,12 +1,17 @@
 import {Nav} from "./components/nav/Nav"
 import './App.module.css'
+import { TopContent } from "./components/top-content/TopContent"
 
 function App() {
 
   return (
     <>
       <Nav/>
-     
+      <TopContent/>
+
+      <h1>hello world</h1>
+      <h2>hello world</h2>
+      <h3>hello world</h3>
     </>
   )
 }
---
+++ MfProdutora/src/components/top-content/TopContent.tsx
@@ -0,0 +1,25 @@
+import styles from "./TopContent.module.css"
+
+export function TopContent(){
+    return(
+        <>
+            <div className={styles.imgFundo}>
+                <div >
+                    <strong>MF  Produtora</strong>
+                
+                   
+                    <span>
+
+                    <p>Somos uma produtora audiovisual que ama contar histórias e transformar sonhos em realidade.</p>
+                    <p>Queremos trazer a melhor qualidade de produção  de conteúdo, realizamos também campanha política e produção de eventos</p>
+
+                    
+                    </span>
+            
+                    <button>Solicitar Orçamento</button>
+                </div>
+            </div>
+        </>
+           
+    )    
+}
