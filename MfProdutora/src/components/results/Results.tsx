a32963fb781300796e2eaa1c376d6efdaeca1eaa
Yure <yure.siilva9@gmail.com>
2023-10-06 21:45:16 -0300

    começando results

--- MfProdutora/src/components/results/Results.tsx
+++ MfProdutora/src/components/results/Results.tsx
@@ -3,11 +3,26 @@ import styles from './Results.module.css'
 export function Results(){
     return(
         <>
-        <div className={styles.background}></div>
-        <div className='container'>
-            <p className='title-section '>Nossos Resultados</p>
-            <h3>Conheça o resultado de<br></br> nossos melhores <span>Projetos</span></h3>
+        <div className={styles.background}>
+            <div className='container'>
+               <p className='title-section'>Nossos Resultados</p>
+            </div>
         </div>
+        <h3 className='container'>Conheça o resultado de<br></br> nossos melhores <span>Projetos</span></h3>
+
+        <article  className='container'>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h4>Jr Soluções em Construções</h4>
+                    <ul>
+                        <li>Tucuruí - PA</li>
+                        <li>Video comercial</li>
+                        <li>20/05/2023</li>
+                    </ul>
+                    </div>
+            </div>
+        </article>
         </>
     )
 }
