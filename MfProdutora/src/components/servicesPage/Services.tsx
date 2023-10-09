aeafe30556c05db84d4a53fca28a007f732c5917
yuro <yure.siilva@gmail.com>
2023-10-09 10:18:36 -0300

    adicionando barra de especialidade

--- MfProdutora/src/components/servicesPage/Services.tsx
+++ MfProdutora/src/components/servicesPage/Services.tsx
@@ -11,35 +11,40 @@ export function Services(){
         <>
             <div className={styles.background}>
                 <div className={styles.containerServices}>
-                <p >Principais Serviços</p>
-                <h3>Conheça Nosso principais<br/><span>Serviços</span> oferecidos</h3>
-                <div className={styles.cards}>
-                    <ServiceCard
-                    img={drone}
-                    type="Filmes e Fotos Aéreas"
-                    content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
-                    />
-                    <ServiceCard
-                    img={audio}
-                    type="Produções e Audiovisual"
-                    content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
-                    />
-                    <ServiceCard
-                    img={video}
-                    type="Comerciais e Inspeções"
-                    content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
-                    />                                     
-                    <ServiceCard
-                    img={post}
-                    type="Social Media e mais"
-                    content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
-                    />
-                    
-                   
-                    
+                    <p >Principais Serviços</p>
+                    <h3>Conheça Nosso principais<br/><span>Serviços</span> oferecidos</h3>
+                    <div className={styles.cards}>
+                        <ServiceCard
+                        img={drone}
+                        type="Filmes e Fotos Aéreas"
+                        content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
+                        />
+                        <ServiceCard
+                        img={audio}
+                        type="Produções e Audiovisual"
+                        content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
+                        />
+                        <ServiceCard
+                        img={video}
+                        type="Comerciais e Inspeções"
+                        content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
+                        />                                     
+                        <ServiceCard
+                        img={post}
+                        type="Social Media e mais"
+                        content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like"
+                        />
+                    </div>
+                    <p>Também temos</p>        
                 </div>
-                </div>
-
+            </div>
+            <div className={styles.carroussel}>
+                <h2>Especialidade 1</h2>
+                <h2>Especialidade 1</h2>
+                <h2>Especialidade 1</h2>
+                <h2>Especialidade 1</h2>
+                <h2>Especialidade 1</h2>
+                <h2>Especialidade 1</h2>
             </div>
         
         </>
