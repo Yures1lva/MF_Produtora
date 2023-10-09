15e3ce45b69b897c23790f80f69f03fc663b1f89
Yure <yure.siilva9@gmail.com>
2023-10-08 22:12:11 -0300

    finalizando results

--- MfProdutora/src/components/results/Component.Result.tsx
+++ MfProdutora/src/components/results/Component.Result.tsx
@@ -1,24 +1,33 @@
 import styles from "./Results.module.css";
+export interface ComponentResultProps{
+    id?: number
+    title: string;
+    local: string;
+    type: string;
+    date: string;
+    imgPerfil: string;
+    img1: string;
+    img2: string;
+}
 
-
-export function ComponetResult(){
+export function ComponetResult({title, local, type, date, imgPerfil, img1, img2}:ComponentResultProps){
     return(
         <>
         <div className="container">
                 <div className={styles.TopContent}>
-                  <div className={styles.imagePerfil}><img  src="../../assets/imgs/clients/client-01.png" alt="" />imgPerfil</div>
+                  <img className={styles.imagePerfil} src={imgPerfil}></img>
                     <div className={styles.descript}>
-                        <h4>Jr Soluções em Construções</h4>
+                        <h4>{title}</h4>
                         <ul>
-                            <li>Tucuruí - PA</li>
-                            <li>Video comercial</li>
-                            <li>20/05/2023</li>
+                            <li>{local}</li>
+                            <li>{type}</li>
+                            <li>{date}</li>
                         </ul>
                     </div>
                 </div>
                 <div className={styles.introImages}>
-                    <div>img1<img src="" alt="" /></div>
-                    <div>img2<img src="" alt="" /></div> 
+                    <div><img src={img1} alt="" /></div>
+                    <div><img src={img2} alt="" /></div> 
                 </div>
             </div>
         </>
--- MfProdutora/src/components/results/Results.tsx
+++ MfProdutora/src/components/results/Results.tsx
@@ -1,6 +1,24 @@
 import { ComponetResult } from './Component.Result'
 import styles from './Results.module.css'
 import divider from '../../App.module.css'
+import {ComponentResultProps} from './Component.Result'
+import imgPerfil from "../../assets/imgs/clients/client01.png"
+import img01A from "../../assets/imgs/clients/01A.png"
+import img01B from "../../assets/imgs/clients/01B.png"
+
+
+const client = [
+    {
+        id: 1,
+        title: "Jr soluções em Construções",
+        local: "Tucuruí - PA",
+        type: "Video Comercial",
+        date: "20/05/2023",
+        imgPerfil: imgPerfil,
+        img1: img01A,
+        img2: img01B,
+    }
+]
 
 export function Results(){
     return(
@@ -12,10 +30,35 @@ export function Results(){
         </div>
         <h3 className='container'>Conheça o resultado de<br></br> nossos melhores <span>Projetos</span></h3>
 
-        <ComponetResult/>
+        <ComponetResult
+        title="Jr soluções em Construções"
+        key={1}
+        type="Video Comercial"
+        date="20/05/2023"
+        local= "Tucuruí - PA"
+        imgPerfil={imgPerfil}
+        img1={img01A}
+        img2={img01B}
+        />
         <div className={divider.divider}></div>
-        <ComponetResult/>
-
+        <ComponetResult
+        title="Jr soluções em Construções"
+        key={1}
+        type="Video Comercial"
+        date="20/05/2023"
+        local= "Tucuruí - PA"
+        imgPerfil={imgPerfil}
+        img1={img01A}
+        img2={img01B}
+        />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         </>
     )
 }
