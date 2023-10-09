801cc02b4af8eea2b2159fd0c85c82dc98ef35b8
Yure <yure.siilva9@gmail.com>
2023-10-09 20:45:57 -0300

    responsividade no slider

--- MfProdutora/src/components/feedback/Carroussel.tsx
+++ MfProdutora/src/components/feedback/Carroussel.tsx
@@ -1,4 +1,4 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import print01 from "../../assets/imgs/feedback/print01.png"
 import print02 from "../../assets/imgs/feedback/print02.png"
 import print03 from "../../assets/imgs/feedback/print03.png"
@@ -13,30 +13,56 @@ import 'swiper/css/navigation';
 import 'swiper/less/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { useState, useEffect } from 'react';
 
-const slides = [print01,print02,print03]
+const slides = [print01,print02,print03,print01,print02,print03]
+
+
 
 export function Carroussel(){
+    const [slidesPerView, setSlidePerView] = useState(3)
+
+    useEffect(() =>{
+        function handleRezie(){
+            if(window.innerWidth < 620){
+                setSlidePerView(1)
+            }else if(window.innerWidth < 920){
+                setSlidePerView(2)
+            }else{
+                setSlidePerView(3)
+            }
+        }
+        handleRezie()
+        window.addEventListener("resize", handleRezie)
+        return ()=> {
+            window.removeEventListener("resize", handleRezie)
+        }
+    }, [])
+
     return(
         <>
           <div className='container' id="feedback">
               <Swiper
-              // install Swiper modules
-              modules={[Navigation, Pagination, Scrollbar, A11y]}
-              spaceBetween={50}
-              slidesPerView={3}
-              navigation
-              pagination={{ clickable: true }}
-              scrollbar={{ draggable: true }}
-                      >
-              <SwiperSlide><div ><img className={style.imgCarroussel} src={print01} /></div></SwiperSlide>
-              <SwiperSlide><div ><img className={style.imgCarroussel} src={print02} /></div></SwiperSlide>
-              <SwiperSlide><div ><img className={style.imgCarroussel} src={print01} /></div></SwiperSlide>
-              <SwiperSlide><div ><img className={style.imgCarroussel} src={print01} /></div></SwiperSlide>
-              <SwiperSlide><div ><img className={style.imgCarroussel} src={print01} /></div></SwiperSlide>
-              <SwiperSlide><div ><img className={style.imgCarroussel} src={print01} /></div></SwiperSlide>
-              <SwiperSlide><div ><img className={style.imgCarroussel} src={print01} /></div></SwiperSlide>
-             
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                spaceBetween={50}
+                slidesPerView={slidesPerView}
+                autoplay={{
+                    delay: 1500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+              >
+              {slides.map((item) => (
+                <SwiperSlide>
+                    <img className={style.imgCarroussel} 
+                    src={item}>
+                    </img>
+                </SwiperSlide>
+              ))}
               </Swiper>
           </div>    
     <br />
