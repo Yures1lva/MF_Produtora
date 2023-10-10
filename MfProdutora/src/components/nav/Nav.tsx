4421b94a01d0792417ac25e697613e346a1d2b3e
yuro <yure.siilva@gmail.com>
2023-10-10 11:25:54 -0300

    navbar estático

--- MfProdutora/src/components/nav/Nav.tsx
+++ MfProdutora/src/components/nav/Nav.tsx
@@ -1,21 +1,35 @@
-import styles from "./Nav.module.css"
+import "./Nav.css"
+
+
+
 
 export function Nav(){
+
+   
+
     return (
         <>
             <header>
-                <nav className="container">
-                    <a className={styles.logo} href=""><img src="" alt="logo" />logo</a>
-                    <ul>
-                    <li><a  href="">Início</a></li>
-                    <li><a  href="#results">Resultados</a></li>
-                    <li><a  href="#services">Serviços</a></li>
-                    <li><a  href="#feedback">Feedback</a></li>
-                    <li><a  href="#team">Equipe</a></li>
-                    <li><a  href="#contact">Contato</a></li>
-                    <li><button>Orçamento</button></li>
-                    </ul>
-                </nav>
+                <div >
+                    <nav className="navigation">
+                        <a className="logo" href="/"><img src="" alt="logo" /></a>
+                        
+                        <div className="navigatioMenu">
+                        {/*<button className="hamburguer" onClick={changeStyle}>
+                        <FaBars/>
+                        </button>*/}
+                            <ul >
+                                <li><a  href="">Início</a></li>
+                                <li><a  href="/results">Resultados</a></li>
+                                <li><a  href="/services">Serviços</a></li>
+                                <li><a  href="/feedback">Feedback</a></li>
+                                <li><a  href="/team">Equipe</a></li>
+                                <li><a  href="/contact">Contato</a></li>
+                                <li><button>Orçamento</button></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </header>
         </>
     )
