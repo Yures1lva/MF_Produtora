ecc49cbf761b115ffdc2cfb3587e4f46f2ee707c
yuro <yure.siilva@gmail.com>
2023-10-17 16:36:40 -0300

    navbar responsivo

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,30 +1,48 @@
 import "./Nav.css"
 import logo from '../../assets/imgs/logo.png'
+import React, {useState, useEffect} from 'react'
+
 
 
 
 
 export function Nav(){
+    const [toggleMenu, setToggleMenu] = useState(false)
+
+    const toggleNav = () =>{
+        setToggleMenu(!toggleMenu)
+    }
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+    useEffect(() =>{
+        const changeWidth = () =>{
+            setScreenWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', changeWidth)
+    }, [])
     return (
         <>
-                    <nav className="navigation container">
-                        <a className="logo" href="/"><img src={logo} alt="logo" /></a>
-                        
-                        <div className="navigatioMenu">
-                        {/*<button className="hamburguer" onClick={changeStyle}>
-                        <FaBars/>
-                        </button>*/}
-                            <ul >
-                                <li><a  href="">Início</a></li>
-                                <li><a  href="/results">Resultados</a></li>
-                                <li><a  href="/services">Serviços</a></li>
-                                <li><a  href="/feedback">Feedback</a></li>
-                                <li><a  href="/team">Equipe</a></li>
-                                <li><a  href="/contact">Contato</a></li>
-                                <li><button>Orçamento</button></li>
-                            </ul>
-                        </div>
-                    </nav>
+                    <header className="navigation">
+                        <nav className="container">
+                            <a className="logo" href="/"><img src={logo} alt="logo" /></a>                        
+                                {(toggleMenu || screenWidth > 860) && (
+                                    <ul className="list">
+                                    <li className="items" ><a  href="">Início</a></li>
+                                    <li className="items" ><a  href="/results">Resultados</a></li>
+                                    <li className="items" ><a  href="/services">Serviços</a></li>
+                                    <li className="items" ><a  href="/feedback">Feedback</a></li>
+                                    <li className="items" ><a  href="/team">Equipe</a></li>
+                                    <li className="items" ><a  href="/contact">Contato</a></li>
+                                    <li className="items" ><a className="buttonNav">Orçamento</a></li>
+                                </ul>  
+                                )
+
+                                }       
+                                <button onClick={toggleNav} className="controlNav">
+                                    menu</button>
+                        </nav>
+                    </header>
         </>
     )
 }
